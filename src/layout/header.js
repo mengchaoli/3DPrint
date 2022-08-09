0372b76bb9dcbd8baa7fb480fbb5d6e16999592f
Mengchao Li <lmcwesdxc@163.com>
2022-08-09 01:34:06 -0700

    responsive header

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,41 +1,71 @@
 // Function Component ( with props and default props ):
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Burger from "react-css-burger";
 
 const Header = (props) => {
   const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth < 501) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
 
   return (
     <div className="header">
-      <div className="header__logo">LOGO</div>
-      {/* burger menu */}
-      <Burger
-        onClick={() => setIsHamburgerActive(!isHamburgerActive)}
-        active={isHamburgerActive}
-        burger="elastic"
-        color="pink"
-        hoverOpacity={0.8}
-        scale={0.7}
-        marginLeft={0}
-        marginTop={0}
-      />
-      {/* burger menu end */}
-      <nav
+      <div className="header__container">
+        <div className="header__logo">LOGO</div>
+        {/* <nav
         className={`header__nav ${
           isHamburgerActive ? "header__nav--open" : ""
         }`}
-      >
-        <ul className="">
-          <li>
+      > */}
+        <ul
+          className={`header__nav ${
+            isHamburgerActive ? "header__nav--open" : ""
+          }`}
+        >
+          <li className="">
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/about-us">About us</Link>
           </li>
+          <li>
+            <Link to="/about-us">About us</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About us</Link>
+          </li>
         </ul>
-      </nav>
+        {/* </nav> */}
+
+        {/* burger menu */}
+        {isMobile && (
+          <Burger
+            onClick={() => setIsHamburgerActive(!isHamburgerActive)}
+            active={isHamburgerActive}
+            burger="elastic"
+            color="pink"
+            hoverOpacity={0.8}
+            scale={0.7}
+            marginLeft={0}
+            marginTop={0}
+          />
+        )}
+        {/* burger menu end */}
+      </div>
     </div>
   );
 };
