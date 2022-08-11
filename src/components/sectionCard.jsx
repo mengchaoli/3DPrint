5dff927498b5d42a4c553abef6d1f8fe63493e17
Mengchao Li <lmcwesdxc@163.com>
2022-08-11 02:16:05 -0700

    finish section selected or not rendering

--- src/components/sectionCard.jsx
+++ src/components/sectionCard.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const SectionCard = (props) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const { name, onClick } = props;
+
+  const handleCardOnClick = () => {
+    setIsSelected(!isSelected);
+    onClick(name);
+  };
+
   return (
-    <div className="section-card">
+    <div
+      className={`section-card ${isSelected ? "selected" : ""}`}
+      onClick={handleCardOnClick}
+    >
       <div className="section-card__img"></div>
-      <div className="section-card__name">{props.name}</div>
+      <div className="section-card__name">{name}</div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import SectionCard from "../components/sectionCard";
+
 const Home = () => {
   const sectionsList = [
     "weather",
@@ -10,13 +11,27 @@ const Home = () => {
     "gaming",
   ];
 
+  const selectedSections = [];
+
+  const handleSectionsChange = (section) => {
+    if (selectedSections.includes(section)) {
+      const idx = selectedSections.indexOf(section);
+      selectedSections.splice(idx, 1);
+    } else {
+      selectedSections.push(section);
+    }
+  };
+
   return (
     <div className="home-wrapper">
       What section(s) would you like to add to your space?
       <div className="home-section-card-wrapper">
         {sectionsList.map((section) => (
-          <div className="center">
-            <SectionCard name={section} />
+          <div className="center" key={section}>
+            <SectionCard
+              name={section}
+              onClick={() => handleSectionsChange(section)}
+            />
           </div>
         ))}
       </div>
